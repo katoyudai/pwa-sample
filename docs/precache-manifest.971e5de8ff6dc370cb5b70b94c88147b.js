5c4868d50a885249d6b765405894bed08ea33f56
katoyudai <yudai.kato@queue-inc.com>
2020-07-29 14:30:37 +0900

    initial

---
+++ docs/precache-manifest.971e5de8ff6dc370cb5b70b94c88147b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16f419cf9800d40ac12",
+    "url": "css/app.700fbeb1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "276fe77fd135e1189ee10d4c4ec73c82",
+    "url": "index.html"
+  },
+  {
+    "revision": "e16f419cf9800d40ac12",
+    "url": "js/app.e1998cdb.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "0184312ec12c5ed50baa5a94a5e13e04",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="start-receiving" @click="startReceiving">START_RECIEVING</div>
+    <div class="topic" @click="topic(`TOPIC`)">TOPIC</div>
+    <div class="stop-receiving" @click="stopReceiving">STOP_RECIEVING</div>
   </div>
 </template>
 
@@ -12,6 +15,18 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    startReceiving() {
+      console.log('forRapper')
+      window.webkit.messageHandlers.FCM.postMessage(`START_RECIEVING`);
+    },
+    topic(topic) {
+      window.webkit.messageHandlers.FCM.postMessage(`TOPIC: ${topic}`);
+    },
+    stopReceiving() {
+      window.webkit.messageHandlers.FCM.postMessage(`STOP_RECIEVING`);
+    }
   }
 }
 </script>
@@ -24,5 +39,29 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.start-receiving {
+  width: 200px;
+  background-color: red;
+  color: white;
+  padding: 8px;
+  margin: 0 auto 20px;
+}
+
+.topic {
+  width: 200px;
+  border: 1px solid black;
+  color: black;
+  padding: 8px;
+  margin: 0 auto 20px;
+}
+
+.stop-receiving {
+  width: 200px;
+  background-color: blue;
+  color: white;
+  padding: 8px;
+  margin: 0 auto 20px;
 }
 </style>
